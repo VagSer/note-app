61519c135fb63c3e317207d4fa10682258c93d6e
Sergey Vagaytsev <thevagser@gmail.com>
2022-11-16 20:28:19 +0700

    Добавление и удаление тегов при редактировании

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -1,8 +1,9 @@
-import { Card, CardContent, CardHeader, IconButton, ButtonGroup, Typography, TextField } from "@mui/material"
+import { Button, Card, CardContent, CardHeader, IconButton, ButtonGroup, Typography, TextField } from "@mui/material"
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
 import ClearIcon from '@mui/icons-material/Clear'
 import CheckIcon from '@mui/icons-material/Check'
+import AddIcon from '@mui/icons-material/Add';
 import { INote } from "./NoteList"
 import { useState } from "react"
 import { useAppDispatch } from "../hook"
@@ -12,6 +13,7 @@ export default function NoteItem({...props}: INote) {
     const dispatch = useAppDispatch()
     const [isEditable, setIsEditable] = useState(false)
     const [currentNote, setCurrentNote] = useState({...props})
+    const [newTag, setNewTag] = useState('')
 
     return (
         isEditable? 
@@ -27,21 +29,57 @@ export default function NoteItem({...props}: INote) {
                 multiline={true}
                 onChange={(event) => setCurrentNote({...currentNote, body: event.target.value})}
             />
+            <section>
+                {currentNote.tags?.map(tag => 
+                <Button 
+                    variant="contained"
+                    sx={{m:'2px'}}
+                    key={tag}
+                    onClick={() => setCurrentNote({...currentNote, tags: currentNote.tags?.filter(t => t !== tag)})}
+                >
+                    {tag}
+                </Button>)}
+            </section>
+            <TextField 
+                label="Тег"
+                sx={{mt: '10px'}}
+                value={newTag}
+                onChange={event => setNewTag(event.target.value)}
+            />
         </CardContent>
         <ButtonGroup>
-            <IconButton onClick={() => {
-                dispatch(editNote(currentNote))
-                setIsEditable(false)
-            }}
+            <Button 
+                color='success'
+                endIcon={<CheckIcon />}
+                onClick={() => {
+                    dispatch(editNote(currentNote))
+                    setIsEditable(false)
+                }}
             >
-                <CheckIcon />
-            </IconButton>
-            <IconButton onClick={() => {
-                setCurrentNote({...props})
-                setIsEditable(false)
-                }}>
-                <ClearIcon />
-            </IconButton>
+                Принять  
+            </Button>
+            <Button
+                endIcon={<AddIcon />}
+                onClick={() => {if(newTag) {
+                    let noteTags = currentNote.tags?.length? [...currentNote.tags, newTag.toLowerCase()] : [newTag.toLowerCase()]
+                    noteTags = [...new Set(noteTags)]
+                    setCurrentNote({...currentNote, tags: [...noteTags]})
+                    setNewTag('')
+                    }
+                }}
+            >
+                Добавить тег
+            </Button>
+            <Button 
+                color='error'
+                endIcon={<ClearIcon />}
+                onClick={() => {
+                    setCurrentNote({...props})
+                    setIsEditable(false)
+                }}
+            >
+                Отменить
+            </Button>
         </ButtonGroup>
         </Card>
         :
@@ -51,14 +89,29 @@ export default function NoteItem({...props}: INote) {
                 <Typography>
                     {props.body}
                 </Typography>
+                <section>
+                    {props.tags?.map(tag => 
+                    <Button 
+                        variant="contained"
+                        sx={{m:'2px'}}
+                        key={tag}
+                    >
+                        {tag}
+                    </Button>)}
+                </section>
             </CardContent>
             <ButtonGroup>
-                <IconButton onClick={() => setIsEditable(true)}>
-                    <EditIcon />
-                </IconButton>
-                <IconButton onClick={() => {if (props.id) dispatch(removeNote(props.id))}}>
-                    <DeleteIcon />
-                </IconButton>
+                <Button 
+                    endIcon={<EditIcon />}
+                    onClick={() => setIsEditable(true)}>
+                    Редактировать
+                </Button>
+                <Button 
+                    color='error'
+                    endIcon={<DeleteIcon />}
+                    onClick={() => {if (props.id) dispatch(removeNote(props.id))}}>
+                    Удалить
+                </Button>
             </ButtonGroup>
         </Card>)
 }
